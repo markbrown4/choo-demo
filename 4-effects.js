beca55518f3333faed2ad009e32f34d4c942cea9
Mark Brown <markbrown4@gmail.com>
2016-09-27 08:28:00 +0100

    Effects demo

---
+++ 4-effects.js
@@ -0,0 +1,39 @@
+const choo = require('choo')
+const html = require('choo/html')
+const http = require('choo/http')
+const app = choo()
+
+app.model({
+  state: {
+    items: []
+  },
+  effects: {
+    fetchItems: (data, state, send, done) => {
+      send('updateItems', [], done)
+      http('/api/items.json', {json: true}, (err, res, body) => {
+        send('updateItems', body.items, done)
+      })
+    }
+  },
+  reducers: {
+    updateItems: (items, state) => ({ items: items })
+  }
+})
+
+const itemView = (item) => html`<li>${item.name}</li>`
+
+const myView = (state, prev, send) => html`
+  <div>
+    <button onclick=${() => send('fetchItems')}>Go get em</button>
+    <ul>
+      ${state.items.map(item => itemView(item))}
+    </ul>
+  </div>
+`
+
+app.router(route => [
+  route('/', myView)
+])
+
+const tree = app.start()
+document.body.appendChild(tree)
