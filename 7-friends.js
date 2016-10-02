cb182751bc1efd8e86a3490ddb62da367d251f9d
Mark Brown <markbrown4@gmail.com>
2016-10-02 11:04:36 +0100

    enter / update

--- 7-friends.js
+++ 7-friends.js
@@ -5,17 +5,25 @@ const app = choo()
 
 app.model({
   state: {
+    data: [4, 8, 15, 16, 23, 42],
     time: new Date().getTime()
   },
   reducers: {
-    updateTime: (data, state) => ({
-      time: new Date().getTime()
-    })
+    update: (data, state) => {
+      const numbers = [];
+      for (let i=0; i<10; i++) {
+        numbers.push(Math.floor(Math.random() * 50) + 1);
+      }
+      return {
+        time: new Date().getTime(),
+        data: numbers
+      }
+    }
   },
   subscriptions: [
     (send, done) => {
       setInterval(() => {
-        send('updateTime', null, done)
+        send('update', null, done)
       }, 1000)
     }
   ]
@@ -25,23 +33,40 @@ const timeView = (state, prev, send) => html`
   <p>${state.time}</p>
 `
 
+let mounted = false;
 const dataVizBiz = (state, prev, send) => {
   function load(el) {
+    mounted = true;
     d3.select(el)
       .selectAll('div')
-      .data([4, 8, 15, 16, 23, 42])
+      .data(state.data)
       .enter()
       .append('div')
+      .attr('class', 'bar')
+      .style('transition', '.5s')
       .style('display', 'inline-block')
+      .style('vertical-align', 'bottom')
       .style('height', (d)=> d + 'px')
       .style('width', '20px')
       .style('margin', '0 2px')
       .style('background', 'deepskyblue')
   }
+
+  function update(el) {
+    d3.selectAll('.bar')
+      .data(state.data)
+      .style('height', (d)=> d + 'px')
+  }
+
   let component = html`
-    <div onload=${load}></div>
+    <div onload=${load} style="height: 51px; line-height: 51px"></div>
   `
-  component.isSameNode = (el) => true
+  component.isSameNode = () => true
+
+  if (mounted) {
+    update(component)
+  }
+
   return component;
 }
 
@@ -49,7 +74,7 @@ const myView = (state, prev, send) => html`
   <div>
     <h1>Sharing is caring</h1>
     ${timeView(state, prev, send)}
-    ${dataVizBiz(state, prev, send)}
+    <div>${dataVizBiz(state, prev, send)}</div>
     ${timeView(state, prev, send)}
   </div>
 `
